373011440935601169582dfd128131c5c5823d32
wangjiong <wj2004021189@163.com>
2021-12-13 21:04:10 +0800

    第一次提交

---
+++ src/test/java/com/msb/Abcde.java
@@ -0,0 +1,13 @@
+package com.msb;
+
+/**
+ * @Auther: wangjiong
+ * @Date: 2021/12/13 - 12 - 13 - 21:02
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class Abcde {
+    public static void main(String[] args) {
+        System.out.println("asasdad");
+    }
+}
